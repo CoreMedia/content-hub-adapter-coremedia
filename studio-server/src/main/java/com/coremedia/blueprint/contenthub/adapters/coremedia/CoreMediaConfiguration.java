ac169173bb232287dab630901e01430ecb3f9bd8
rwaterma <rolf.watermann@coremedia.com>
2020-10-06 16:44:00 +0200

    Migrate to Plugin
    CMS-18303

--- studio-server/src/main/java/com/coremedia/blueprint/contenthub/adapters/coremedia/CoreMediaConfiguration.java
+++ studio-server/src/main/java/com/coremedia/blueprint/contenthub/adapters/coremedia/CoreMediaConfiguration.java
@@ -1,28 +1,25 @@
 package com.coremedia.blueprint.contenthub.adapters.coremedia;
 
+import com.coremedia.cap.common.CapConnection;
 import com.coremedia.cap.content.ContentRepository;
-import com.coremedia.cap.undoc.common.spring.CapRepositoriesConfiguration;
+import com.coremedia.cms.common.plugins.pluginbeans.plugin.CapConnectionPluginBeansPluginConfiguration;
 import com.coremedia.contenthub.api.ContentHubAdapterFactory;
 import com.coremedia.contenthub.api.column.ColumnProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({CapRepositoriesConfiguration.class})
+@Import({CapConnectionPluginBeansPluginConfiguration.class})
 public class CoreMediaConfiguration {
-
-  @Autowired
-  private ContentRepository contentRepository;
-
   @Bean
   public ColumnProvider coreMediaContentHubColumnModelProvider() {
     return new CoreMediaColumnProvider();
   }
 
   @Bean
-  public ContentHubAdapterFactory coreMediaContentHubAdapterFactory() {
+  public ContentHubAdapterFactory coreMediaContentHubAdapterFactory(CapConnection capConnection) {
+    ContentRepository contentRepository = capConnection.getContentRepository();
     return new CoreMediaContentHubAdapterFactory(contentRepository);
   }
 }
